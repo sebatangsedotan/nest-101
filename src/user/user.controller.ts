89126179c530c0561a9711bf769c1a747fbfec2e
zen <12.zeinirfansyah@gmail.com>
2025-11-29 18:05:06 +0700

    http-request: decorator request for express.Request (not recomended)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import express from 'express';
 
 @Controller('/api/v1/user')
 export class UserController {
+  @Get('/:id')
+  getUserById(@Req() request: express.Request): string {
+    return `Get user with ID: ${request.params.id}`;
+  }
+
   @Get('/sample')
   getUser() {
     return { id: 1, name: 'John Doe' };
